753a64f70591d327b63ca8f72a9e154c4ac5fa1d
RealMeKaNiK <dautovic2000@gmail.com>
2022-10-05 13:22:01 +0200

    commitus

--- VisualStudio_Projects/PPPK_Vjezba_03/Model/Database.cs
+++ VisualStudio_Projects/PPPK_Vjezba_03/Model/Database.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PPPK_Vjezba_03.Dal;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -8,8 +9,7 @@ using System.Threading.Tasks;
 namespace PPPK_Vjezba_03.Model
 {
     public class Database
-    {
-        private readonly Lazy<DataSet> dataSet;        
+    {      
         public string Name { get; set; }
         public override string ToString() => Name;
     }
