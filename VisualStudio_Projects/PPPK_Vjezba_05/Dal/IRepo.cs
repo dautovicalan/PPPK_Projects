3ef127413f37758c110f61d48068ec5e6271f0d2
RealMeKaNiK <dautovic2000@gmail.com>
2022-10-17 13:17:39 +0200

    creating new project 05

---
+++ VisualStudio_Projects/PPPK_Vjezba_05/Dal/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPK_Vjezba_05.Dal
+{
+    // https://www.codeguru.com/dotnet/windows-presentation-foundations-observablecollection-made-easy/
+    public interface IRepo
+    {
+        ObservableCollection<string> GetStudents();
+        ObservableCollection<string> GetProfessors();
+        ObservableCollection<string> GetSubjects();
+    }
+}
---
+++ VisualStudio_Projects/PPPK_Vjezba_05/Models/Professor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPK_Vjezba_05.Models
+{
+    public class Professor
+    {
+    }
+}
