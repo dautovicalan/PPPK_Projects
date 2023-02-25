1fa5a660baf5b4f40f2f99fcdd499d8978348d60
Alan <84068754+dautovicalan@users.noreply.github.com>
2023-02-25 13:40:05 +0100

    vjezba 13

---
+++ Vjezba_13_Alan_Dautovic_3RP1/CosmosDbOsoba/Dao/CosmosDbServiceProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CosmosDbOsoba.Dao
+{
+    public class CosmosDbServiceProvider
+    {
+        private const string DatabaseName = "People";
+        private const string ContainerName = "OsobaManager";
+        private const string Account = "https://adautovic.documents.azure.com:443/";
+        private const string Key = "OphvRvWDnHuPiOGhOcq6rQUwlfzkPIxgw7fEDF2ehgkOZ45LizpWjHTIzYKycjZzzrPha2f9neubACDbN0R5Lg==";
+
+        private static ICosmosDbService cosmosDbService;
+
+        public static ICosmosDbService CosmosDbService { get => cosmosDbService; }
+
+        public static async Task Init()
+        {
+            CosmosClient cosmosClient = new CosmosClient(Account, Key);
+            cosmosDbService = new CosmosDbService(cosmosClient, DatabaseName, ContainerName);
+            DatabaseResponse databaseResponse
+                = await cosmosClient.CreateDatabaseIfNotExistsAsync(DatabaseName);
+            await databaseResponse.Database.CreateContainerIfNotExistsAsync(ContainerName, "/id");
+        }
+    }
+}
---
+++ Vjezba_13_Alan_Dautovic_3RP1/CosmosDbOsoba/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CosmosDbOsoba.Models
+{
+    public class Person
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [Required]
+        [JsonProperty(PropertyName = "firstname")]
+        public string FirstName { get; set; }
+        [Required]
+        [JsonProperty(PropertyName = "lastname")]
+        public string LastName { get; set; }
+        [Required]
+        [JsonProperty(PropertyName = "age")]
+        public int Age { get; set; }
+        [Required]
+        [JsonProperty(PropertyName = "birthday")]
+        public DateTime Birthday { get; set; }
+    }
+}
